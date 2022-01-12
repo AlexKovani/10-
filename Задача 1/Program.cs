7613d4cd509971d1d39f5a14dd23e25808c4d83c
canya <canya@DESKTOP-0RBMO97>
2022-01-12 22:36:07 +0300

    Добавьте файлы проекта.

---
+++ Задача 1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_1
+
+   //Угол задан с помощью целочисленных значений gradus - градусов, min - угловых минут, sec - угловых секунд.
+   //Реализовать класс, в котором указанные значения представлены в виде свойств.
+   //Для свойств предусмотреть проверку корректности данных. Класс должен содержать конструктор для установки начальных значений,
+   //а также метод ToRadians для перевода угла в радианы.Создать объект на основе разработанного класса.
+   //Осуществить использование объекта в программе.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Введите значение угла в градусах: ");
+                int gradus = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите значение минут: ");
+                int min = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите значение секунд: ");
+                int sec = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+                Corner one = new Corner(gradus, min, sec);
+                one.ToRadians();
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+           
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Задача 1/Сorner.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_1
+{
+    class Corner
+    {
+        int gradus;
+        int min;
+        int sec;
+        public int Gradus 
+        {
+            get
+            {
+                return gradus;
+            }
+
+            set
+            {
+                if (value<0)
+                {
+                   value = 0;
+                }
+                else
+                {
+                    gradus = value;
+                    
+                }  
+            }      
+        }
+
+        public int Min // угловые минуты
+        {
+            get
+            {
+                return min;
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+
+                    Console.WriteLine("Введите положительное значение!");
+                    value = 0;
+                }
+                else
+                {
+                   
+                            Gradus += value / 60;
+                            min = value % 60;
+                }
+            }
+
+        }
+
+        public int Sec  // угловые секунд
+        {
+            get
+            {
+                return sec;
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+
+                    Console.WriteLine("Введите положительное значение!");
+                    value = 0;
+                }
+                else
+                {
+                    
+                            Min += value / 60;
+                            sec = value % 60;
+                            Console.WriteLine("{0}° = {1} оборот и {2}°", Gradus, Gradus / 360, Gradus % 360);
+                        
+                }
+            }
+
+        }
+
+        public Corner(int gradus, int min =0, int sec =0)
+        {
+            Gradus = gradus;
+            Min = min;
+            Sec = sec;
+        }
+        public void ToRadians()
+        {
+            if (gradus<0 || min<0|| sec<0)
+            {
+                Console.WriteLine("Введите положительное значение!");
+            }
+            else
+            {
+                double x = gradus * Math.PI / 180 + min * Math.PI / (180 * 60) + sec * Math.PI / (180 * 60 * 60);
+                Console.WriteLine("{0}° = {1} радиан", Gradus, Math.Round(x, 4));
+            }
+            
+        }
+    }
+}
